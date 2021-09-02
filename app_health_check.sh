c7a34d2c0c169b5510cfc6a6799ba6b12cb8d60d
root <root@centos-docker.int.sbisoft.ru>
2021-09-02 10:56:15 +0300

    fix app_health_check.sh

--- app_health_check.sh
+++ app_health_check.sh
@@ -6,7 +6,7 @@ check_var=$?
 
 if [ $check_var -ne 0 ]
   then
-    echo "Fail, health test NOT PASSED, application deploy, but not response"
+    echo "Fail, health test NOT PASSED, application deploy, but not response"; exit 1
   else
     echo "Health test PASSED, application work fine "
 fi
